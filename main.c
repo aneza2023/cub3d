59b2ecf61b461a788291ef8579c4f33f5861076d
Olha Chernova <olcherno@c2r3s3.42prague.com>
2025-11-26 18:10:21 +0100

    18:07

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: olcherno <olcherno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 22:09:42 by anezkahavra       #+#    #+#             */
-/*   Updated: 2025/11/26 18:07:09 by olcherno         ###   ########.fr       */
+/*   Updated: 2025/11/26 18:10:13 by olcherno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,7 @@ int	main(int argc, char *argv[])
 {
 	t_map *map1;
 
-	map = NULL;
-	if (argc != 2)
-	{
-		ft_putstr_fd("Program accepts only single argument in *.cub format\n", STDERR_FILENO);
-		return (1);
-	}
+
 	else
 		parse_crossroad(argv[1], &map);
 	free_map(&map);
